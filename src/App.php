efbb4e2e8b25e6af2e13e9228858462665b12719
Tendry <tendryzephyrin@gmail.com>
2025-03-11 06:26:53 +0100

    Modification du App.php pour avoir une instance du QueryBuilder

--- src/App.php
+++ src/App.php
@@ -3,14 +3,16 @@
 namespace App;
 
 use Core\DataBase\DataBase;
+use Core\DataBase\QueryBuilder;
 
 class App 
 {
 
      private static $_instance;
      private static $_db;
+     private static $_builder;
 
-     public function getInstance(): self
+     public static function getInstance(): self
      {
           if (self::$_instance == null) self::$_instance = new self();
           return self::$_instance;
@@ -20,6 +22,12 @@ class App
      {
           if (self::$_db == null) self::$_db = new DataBase(DB_NAME);
           return self::$_db;
+     }
+
+     public function getQueryBuilder()
+     {
+          if (self::$_builder == null) self::$_builder = new QueryBuilder($this->getDb());
+          return self::$_builder;
      }
 
      public function getEntity(string $class)
