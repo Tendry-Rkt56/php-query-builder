ebcea981cd969567748832a7c78ccaae8b5de13c
Tendry <tendryzephyrin@gmail.com>
2025-03-11 06:43:24 +0100

    Création des méthodes de base dans le Entity.php

--- src/Entity/Entity.php
+++ src/Entity/Entity.php
@@ -8,9 +8,21 @@ use Core\DataBase\QueryBuilder;
 class Entity extends QueryBuilder
 {
 
+     protected string $table = '';
+
      public function __construct(private DataBase $db)
      {
           parent::__construct($db);
+     }
+
+     public function findAll()
+     {
+          return $this->table($this->table)->get();
+     }
+
+     public function find(int $id)
+     {
+          return $this->table($this->table)->where('id', '=', $id)->get();
      }
 
 
