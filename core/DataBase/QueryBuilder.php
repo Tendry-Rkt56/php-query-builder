5311a1c6c0eace56f222399e623005de0569f02e
Tendry <tendryzephyrin@gmail.com>
2025-03-11 06:09:13 +0100

    Mise en place des propriétés dans le QueryBuilder.php

--- core/DataBase/QueryBuilder.php
+++ core/DataBase/QueryBuilder.php
@@ -5,6 +5,19 @@ namespace Core\DataBase;
 class QueryBuilder 
 {
 
+     private DataBase $database;
+     private string $table;
+     private string $columns = '*';
+     private array $where = [];
+     private array $bindings = [];
+     private string $orderBy = '';
+     private string $limit = '';
+
+     public function __construct(DataBase $pdo)
+     {
+         $this->database = $pdo;
+     }
+
      
 
 }
