699bd38397a609f56043f05505464fa8ee0f11f6
Tendry <tendryzephyrin@gmail.com>
2025-03-11 06:11:43 +0100

    Création des principaux méthodes dans le QueryBuilder.php

--- core/DataBase/QueryBuilder.php
+++ core/DataBase/QueryBuilder.php
@@ -18,6 +18,35 @@ class QueryBuilder
          $this->database = $pdo;
      }
 
-     
+     public function table(string $table): self
+     {
+         $this->table = $table;
+         return $this;
+     }
+
+     public function select(array|string $columns = '*'): self
+     {
+         $this->columns = is_array($columns) ? implode(', ', $columns) : $columns;
+         return $this;
+     }
+
+     public function where(string $column, string $operator, mixed $value): self
+     {
+         $this->where[] = "$column $operator ?";
+         $this->bindings[] = $value;
+         return $this;
+     }
+
+     public function orderBy(string $column, string $direction = 'ASC'): self
+     {
+         $this->orderBy = "ORDER BY $column $direction";
+         return $this;
+     }
+
+     public function limit(int $limit): self
+     {
+         $this->limit = "LIMIT $limit";
+         return $this;
+     }
 
 }
