0874979d28f1e796986359488c64418c54a6e2c0
Tendry <tendryzephyrin@gmail.com>
2025-03-14 14:01:09 +0100

    Création des templates de base

---
+++ templates/category/edit.html.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Edition catégorie</title>
+</head>
+<body>
+     {# Edition de catégorie #}
+</body>
+</html>
