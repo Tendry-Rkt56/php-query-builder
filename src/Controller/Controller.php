d8800c32b5f6d25cca81ad1ff7720fac7466465e
Tendry <tendryzephyrin@gmail.com>
2025-03-14 13:46:39 +0100

    Création de la méthode render() dans le Controller.php

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -10,4 +10,10 @@ class Controller
           if (session_status() == PHP_SESSION_NONE) session_start();
      }
 
+     protected function render(string $template = '', array $data = [])
+     {
+          extract($data);
+          require_once '../templates/'.$template;
+     }
+
 }
